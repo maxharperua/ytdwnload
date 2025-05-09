5782190b314f1a8e4c6b552da669e9f0ce9e297a
MaxHarper <islowget@gmail.com>
2025-05-09 18:19:01 +0300

    Обновлено представление app.blade.php: изменен заголовок страницы, добавлены мета-теги для SEO и Open Graph, а также подключен скрипт для Яндекс.Метрики. Удалено представление layouts/app.blade.php.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,39 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>YouTube Конвертер</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" type="image/svg+xml" href="/logo.svg">
+    <title>Скачать видео и музыку с YouTube, ВКонтакте, RuTube, Shorts — Бесплатно и без регистрации</title>
+    <meta name="description" content="Скачивайте видео, музыку и фото с YouTube, ВКонтакте, RuTube, Shorts бесплатно и без регистрации. Быстрый онлайн загрузчик — просто вставьте ссылку и скачайте нужный файл!">
+    <meta name="keywords" content="скачать видео, скачать музыку, скачать фото, youtube downloader, вконтакте, rutube, shorts, загрузчик, онлайн, бесплатно, без регистрации">
+    <link rel="canonical" href="https://ytload.ru/">
+    <!-- Open Graph и Twitter -->
+    <meta property="og:title" content="Скачать видео и музыку с YouTube, ВКонтакте, RuTube, Shorts — Бесплатно и без регистрации">
+    <meta property="og:description" content="Быстрое и бесплатное скачивание видео, музыки и фото с YouTube, ВКонтакте, RuTube, Shorts.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://ytload.ru/">
+    <meta property="og:image" content="/preview.png">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Скачать видео и музыку с YouTube, ВКонтакте, RuTube, Shorts — Бесплатно и без регистрации">
+    <meta name="twitter:description" content="Скачивайте видео, музыку и фото с YouTube, ВКонтакте, RuTube, Shorts онлайн, быстро и бесплатно.">
+    <meta name="twitter:image" content="/preview.png">
 </head>
 <body>
     <div id="app"></div>
+    <noscript><div><img src="https://mc.yandex.ru/watch/101710991" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <script type="text/javascript" >
+       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+       m[i].l=1*new Date();
+       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+       ym(101710991, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+       });
+    </script>
 </body>
 </html> 
