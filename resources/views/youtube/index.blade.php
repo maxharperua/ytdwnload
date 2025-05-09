d3f82e5196aed9eea7bf78fca898d1f40d870933
MaxHarper <islowget@gmail.com>
2025-05-09 10:50:45 +0300

    Добавлено подключение Yandex.RTB для отображения рекламных блоков в представлении youtube.index. Внедрены необходимые скрипты и контейнер для рендеринга рекламы.

--- resources/views/youtube/index.blade.php
+++ resources/views/youtube/index.blade.php
@@ -323,6 +323,9 @@
             }
         }
     </style>
+    <!-- Yandex.RTB -->
+    <script>window.yaContextCb=window.yaContextCb||[]</script>
+    <script src="https://yandex.ru/ads/system/context.js" async></script>
 </head>
 <body>
     <div class="preloader-overlay" id="preloader" style="display:none;">
@@ -477,5 +480,32 @@
             }
         });
     </script>
+    <!-- Yandex.RTB R-A-15418559-1 -->
+    <div id="yandex_rtb_R-A-15418559-1"></div>
+    <script>
+    window.yaContextCb.push(() => {
+        Ya.Context.AdvManager.render({
+            "blockId": "R-A-15418559-1",
+            "renderTo": "yandex_rtb_R-A-15418559-1"
+        })
+    })
+    </script>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    
+        ym(101710991, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/101710991" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+ <!-- /Yandex.Metrika counter -->
 </body>
 </html> 
